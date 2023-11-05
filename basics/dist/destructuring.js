51e8227cf4b6a446fd10f6187c9f3bd58f5476b1
Sawom <asawom250@gmail.com>
2023-11-05 15:49:38 +0600

    added

---
+++ basics/dist/destructuring.js
@@ -0,0 +1,24 @@
+"use strict";
+{
+    // destructuring
+    const user = {
+        id: 345,
+        name: {
+            fname: 'abdur',
+            mname: 'rashid',
+            lname: 'sawom',
+        },
+        contactno: '0171000000',
+        address: 'uganda'
+    };
+    // object destructure
+    const { contactno, name: { mname: midname } } = user;
+    // name:{mname: midname} name change korlo
+    // array destructure
+    const myFriends = ['mir', 'zafar', 'modon', 'tanha', 'tamanna', 'rinti'];
+    const [a, b, c, ...rest] = myFriends;
+    // a = "mir" , b = 'zafar' , c = 'modon'
+    // ...rest = bakishob zotogula thake shb ...rest e ache
+    const [, , best] = myFriends;
+    // c er value change kore best rakhlam. eta kora zay. varriable er nam redeclear kora
+}
